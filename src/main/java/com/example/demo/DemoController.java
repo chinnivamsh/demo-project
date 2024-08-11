01bb34fe580fd153f2e0b4af54395454cbf8e812
chinnivamshi <121663694+chinnivamsh@users.noreply.github.com>
2024-08-11 10:48:09 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getEndpoint(){
+        return new ResponseEntity<>("Hi from Demo app", HttpStatus.OK);
+    }
+}
